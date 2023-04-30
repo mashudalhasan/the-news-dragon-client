58cbcf3f1c60ffa77cf2db204324d3c8105bbf8a
mashudalhasan <mashudalhasan554@gmail.com>
2023-04-30 17:02:24 +0600

    added right navigation page and left navigation

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <Container>
+    <Container className="mt-4">
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
@@ -13,9 +15,37 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-      <div>
-        <Button className="rounded-0" variant="danger">Latest</Button>
+      <div className="latest-info p-4">
+        <div className="d-flex">
+          <Button className="rounded-0" variant="danger">
+            Latest
+          </Button>
+          <Marquee speed={50} pauseOnHover={true}>
+            I can be a React component, multiple React components, or just some
+            text.... I can be a React component, multiple React components, or
+            just some text.... I can be a React component, multiple React
+            components, or just some text....
+          </Marquee>
+        </div>
       </div>
+      <Navbar collapseOnSelect expand="lg" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button className="rounded-0" variant="dark">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from "../../../assets/bg.png";
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Navigation</h2>
+  return (
+    <div className="mt-4">
+      <h4>Login With</h4>
+      <Button className="mb-2 w-100" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button className="w-100" variant="outline-dark">
+        <FaGithub /> Login with Github
+      </Button>
+      <div className="mt-4">
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div className="position-relative">
+        <img className="w-100" src={bg} alt="" />
+        <div className="position-absolute top-50 start-50 translate-middle text-white text-center">
+          <h4>Create an Amazing Newspaper</h4>
+          <p className="mt-2 mb-3">
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Button className="rounded-0" variant="danger">
+            Learn More
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
